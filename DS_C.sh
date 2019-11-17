0d43b96da1802d4d65e7326b1f12b8e4df446ffb
Efan Andrian <efan.andrian05@gmail.com>
2019-11-17 20:22:40 +0700

    ds

---
+++ DS_C.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"C:\ds_candiumbul\kill_DS.exe" $1 &
+git pull
+"C:\ds_candiumbul\DesktopService.exe" $1 &
+
+
---
+++ DS_D.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+"D:\ds_candiumbul\kill_DS.exe" $1 &
+git pull
+"D:\ds_candiumbul\DesktopService.exe" $1 &
+
